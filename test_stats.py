d268643bd0054a26fcb75181fa3736519c83246a
Emily Colo <ejbrunjes@wisc.edu>
2015-08-27 13:40:12 -0500

    change to nosetesting

--- test_stats.py
+++ test_stats.py
@@ -1,10 +1,16 @@
-import stats
+from stats import mean
+from nose.tools import assert_equal
 
 #testing our new mean function
 def test_mean():
-	assert(mean([2,4]) ==3)
-test_mean()
+	assert_equal(mean([2,4]), 3)
+#test_mean()
 
 def test_float_mean():
-	assert(mean([1,2]) ==1.5)
-test_float_mean()
+	assert_equal(mean([1,2]), 1.5)
+#test_float_mean()
+
+def test_long_mean():
+	assert_equal(mean([2,4,6,8]), 5)
+#test_long_mean()
+

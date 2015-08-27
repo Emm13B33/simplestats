99782609069162772312b9e58a452330fa6c0a21
Emily Colo <ejbrunjes@wisc.edu>
2015-08-27 13:25:17 -0500

    Ammended stats and created test_stats

--- stats.py
+++ stats.py
@@ -1,7 +1,10 @@
+# simple example of a function that computes the mean
 def mean(vals):
 	total = sum(vals)
 	length = len(vals)
 	return total/length
 
 print mean([2,4])
+
+
 
---
+++ test_stats.py
@@ -0,0 +1,10 @@
+import stats
+
+#testing our new mean function
+def test_mean():
+	assert(mean([2,4]) ==3)
+test_mean()
+
+def test_float_mean():
+	assert(mean([1,2]) ==1.5)
+test_float_mean()
